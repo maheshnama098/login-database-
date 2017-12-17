b3b2855847031d3754c861c99cb54a49ce4e383b
Joel Shaw <jshaw@rvadesignsolutions.com>
2017-12-17 18:59:44 -0500

    Setup Gulp server

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp'),
       concat = require('gulp-concat'),
+      gls = require('gulp-live-server'),
       htmlmin = require('gulp-htmlmin'),
+      imagemin = require('gulp-imagemin'),
       sass = require('gulp-ruby-sass'),
       sourcemaps = require('gulp-sourcemaps');
 
@@ -8,6 +10,27 @@ gulp.task('html', function(){
     return gulp.src('./src/**/*.html')
         .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('images', function(){
+    return gulp.src('./src/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/img'));
+});
+
+gulp.task('scripts', function(){
+    return gulp.src('./src/js/*.js')
+        .pipe(concat('build.js'))
+        .pipe(gulp.dest('./dist/js'))
+});
+
+gulp.task('serve', function(){
+    var server = gls.static('dist', 3000);
+    server.start();
+
+    gulp.watch(['dist/**/*.css', 'dist/**/*.html'], function(file){
+        server.notify.apply(server, [file]);
+    });
 });
 
 gulp.task('styles', function(){
@@ -20,4 +43,4 @@ gulp.task('styles', function(){
         .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('default', ['html', 'styles']);
+gulp.task('default', ['html', 'images', 'scripts', 'styles', 'serve']);
