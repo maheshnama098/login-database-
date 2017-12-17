b9a0bd58e8a49bfe4fbeb30222353c2b468daa62
Joel Shaw <jshaw@rvadesignsolutions.com>
2017-12-17 18:48:42 -0500

    Setup sass stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 const gulp = require('gulp'),
       concat = require('gulp-concat'),
       htmlmin = require('gulp-htmlmin'),
-      sass = require('gulp-ruby-sass');
+      sass = require('gulp-ruby-sass'),
+      sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('html', function(){
     return gulp.src('./src/**/*.html')
@@ -9,4 +10,14 @@ gulp.task('html', function(){
         .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', ['html']);
+gulp.task('styles', function(){
+    sass('./src/sass/*.scss', {
+            sourcemap: true, 
+            style: 'compressed'
+        })
+        .on('error', sass.logError)
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('default', ['html', 'styles']);
